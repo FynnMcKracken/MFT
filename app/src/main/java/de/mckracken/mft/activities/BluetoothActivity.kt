441dce79724c65a41cc17a8461f8a4e5219bdd2c
Fynn Theiß <fynn.theiss@web.de>
2019-06-25 07:13:09 +0200

    Moved Bluetoothfragment to activity

--- app/src/main/java/de/mckracken/mft/activities/BluetoothActivity.kt
+++ app/src/main/java/de/mckracken/mft/activities/BluetoothActivity.kt
@@ -1,51 +1,42 @@
-package de.mckracken.mft.fragments
+package de.mckracken.mft.activities
 
 import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
-import android.view.ViewGroup
-import android.widget.ProgressBar
-import android.widget.Toast
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import de.mckracken.mft.MainActivity
 import de.mckracken.mft.MultinoxApplication
 import de.mckracken.mft.R
-import de.mckracken.mft.manager.DMXManager
+import de.mckracken.mft.fragments.NewBluetoothDeviceRecyclerViewAdapter
 import de.mckracken.mft.manager.NewBluetoothManager
-import de.mckracken.mft.viewmodel.ChannelsViewModel
-import kotlinx.android.synthetic.main.fragment_bluetooth.view.*
+import kotlinx.android.synthetic.main.activity_bluetooth.*
 
-class NewBluetoothFragment(private val activity : MainActivity) : Fragment() {
+class BluetoothActivity : AppCompatActivity() {
 
-    private var bluetoothManager = (activity.application as MultinoxApplication).bluetoothManager
+    private lateinit var bluetoothManager: NewBluetoothManager
     private val devices : ArrayList<BluetoothDevice> = ArrayList()
     private lateinit var recyclerViewAdapter : NewBluetoothDeviceRecyclerViewAdapter
-    private var dmxManager =  (activity.application as MultinoxApplication).dmxManager
-    private val channelsViewModel = ViewModelProviders.of(activity).get(ChannelsViewModel::class.java)
     private val filter = IntentFilter(BluetoothDevice.ACTION_FOUND)
     private val receiver = object : BroadcastReceiver() {
 
         override fun onReceive(context: Context, intent: Intent) {
             when(intent.action) {
                 BluetoothDevice.ACTION_FOUND -> {
-                    Log.d("NewBluetoothFragment()", "BluetoothDevice.ACTION_FOUND")
+                    Log.d("BluetoothActivity()", "BluetoothDevice.ACTION_FOUND")
                     devices.add(intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE))
                     recyclerViewAdapter.setDevices(devices)
                 }
                 BluetoothDevice.ACTION_NAME_CHANGED -> {
                     val renamedDevice : BluetoothDevice = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)
-                    Log.d("NewBluetoothFragment", "BluetoothDevice.ACTION_NAME_CHANGED: " + renamedDevice.name)
+                    Log.d("BluetoothActivity", "BluetoothDevice.ACTION_NAME_CHANGED: " + renamedDevice.name)
                     for (device in devices) {
                         if (device.address == renamedDevice.address) {
                             devices[devices.indexOf(device)] = renamedDevice
@@ -59,42 +50,77 @@ class NewBluetoothFragment(private val activity : MainActivity) : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        filter.addAction(BluetoothDevice.ACTION_NAME_CHANGED)
-        activity.registerReceiver(receiver, filter)
-        channelsViewModel.getChannel(42).observe(activity, Observer { t ->
-            //Toast.makeText(context, "Channel updated", Toast.LENGTH_SHORT).show()
-            //Log.d("NewBluetoothFragment", "Channel updated")
+        setContentView(R.layout.activity_bluetooth)
+        bluetoothManager = (application as MultinoxApplication).bluetoothManager
 
-        })
-        Log.d("NewBluetoothFragment", "onCreate()")
+        setSupportActionBar(bluetooth_toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        supportActionBar?.title = "Bluetooth"
+
+        filter.addAction(BluetoothDevice.ACTION_NAME_CHANGED)
+
+        bluetooth_rv.layoutManager = LinearLayoutManager(this)
+        bluetooth_rv.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
+        recyclerViewAdapter = NewBluetoothDeviceRecyclerViewAdapter(this, ArrayList(), bluetoothManager)
+        bluetooth_rv.adapter = recyclerViewAdapter
+
+        bluetooth_button_on.setOnClickListener { bluetoothManager.bluetoothOn(this) }
+        bluetooth_button_off.setOnClickListener {
+            bluetooth_spinner.visibility = View.INVISIBLE
+            devices.clear()
+            recyclerViewAdapter.setDevices(devices)
+            bluetoothManager.bluetoothOff()
+        }
+        bluetooth_button_discover.setOnClickListener {
+            bluetooth_spinner.visibility = View.VISIBLE
+            bluetooth_spinner.bringToFront()
+            devices.clear()
+            recyclerViewAdapter.setDevices(devices)
+            bluetoothManager.discoverDevices()
+        }
+        bluetooth_button_show.setOnClickListener {
+            bluetooth_spinner.visibility = View.VISIBLE
+            devices.clear()
+            recyclerViewAdapter.setDevices(devices)
+            recyclerViewAdapter.setDevices(bluetoothManager.showPairedDevices() ?: ArrayList())
+        }
 
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view: View = inflater.inflate(R.layout.fragment_bluetooth, container, false)
-        val recyclerView = view.findViewById<RecyclerView>(R.id.fragment_bluetooth_rv)
-        val spinner = view.findViewById<ProgressBar>(R.id.fragment_bluetooth_spinner)
-        recyclerViewAdapter = NewBluetoothDeviceRecyclerViewAdapter(activity, ArrayList(), bluetoothManager)
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when(item?.itemId) {
+            android.R.id.home -> onBackPressed()
+        }
+        return true
+    }
+
+    /*override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view: View = inflater.inflate(R.layout.activity_bluetooth, container, false)
+        val recyclerView = view.findViewById<RecyclerView>(R.id.bluetooth_rv)
+        val spinner = view.findViewById<ProgressBar>(R.id.bluetooth_spinner)
+        recyclerViewAdapter =
+            NewBluetoothDeviceRecyclerViewAdapter(activity, ArrayList(), bluetoothManager)
 
         recyclerView.layoutManager = LinearLayoutManager(context)
         recyclerView.addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
         recyclerView.adapter = recyclerViewAdapter
 
-        view.fragment_bluetooth_button_on.setOnClickListener { bluetoothManager.bluetoothOn(activity) }
-        view.fragment_bluetooth_button_off.setOnClickListener {
+        view.bluetooth_button_on.setOnClickListener { bluetoothManager.bluetoothOn(activity) }
+        view.bluetooth_button_off.setOnClickListener {
             spinner.visibility = View.INVISIBLE
             devices.clear()
             recyclerViewAdapter.setDevices(devices)
             bluetoothManager.bluetoothOff()
         }
-        view.fragment_bluetooth_button_discover.setOnClickListener {
+        view.bluetooth_button_discover.setOnClickListener {
             spinner.visibility = View.VISIBLE
             spinner.bringToFront()
             devices.clear()
             recyclerViewAdapter.setDevices(devices)
             bluetoothManager.discoverDevices()
         }
-        view.fragment_bluetooth_button_show.setOnClickListener {
+        view.bluetooth_button_show.setOnClickListener {
             spinner.visibility = View.VISIBLE
             devices.clear()
             recyclerViewAdapter.setDevices(devices)
@@ -115,21 +141,15 @@ class NewBluetoothFragment(private val activity : MainActivity) : Fragment() {
 
         return view
 
-    }
+    }*/
 
     override fun onPause() {
         super.onPause()
-        activity.unregisterReceiver(receiver)
+        this.unregisterReceiver(receiver)
     }
 
     override fun onResume() {
         super.onResume()
-        activity.registerReceiver(receiver, filter)
+        this.registerReceiver(receiver, filter)
     }
-
-//    override fun onDestroy() {
-//        super.onDestroy()
-//        activity.unregisterReceiver(receiver)
-//        Log.i("NewBluetoothFragment", "onDestroy()")
-//    }
 }
